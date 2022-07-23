2908c10449c16fecbb4332fe129bef1bd4ccc595
Lucas Scharenbroch <79030344+lucasscharenbroch@users.noreply.github.com>
2022-07-22 19:50:23 -0500

    Update 7_Reverse_Integer.java

--- 7_Reverse_Integer.java
+++ 7_Reverse_Integer.java
@@ -2,30 +2,22 @@ class Solution {
     public int reverse(int x) {
         int result = 0;    
         int sign = (x > 0) ? 1 : -1;
-        
+
         if(sign == -1) {
             x = -x;
         } 
-        
+
         while(x != 0) {
-            int digitValue = (x % 10) * sign;
-            
             // check for overflow
-            if(sign == 1) {
-                if(result > Integer.MAX_VALUE / 10 || result * 10 + digitValue < 0) {
-                    return 0;
-                }
-            } else {
-                if(result < Integer.MIN_VALUE / 10 || result * 10 + digitValue > 0) {
-                    return 0;
-                }
+            if(result > Integer.MAX_VALUE / 10 || result < Integer.MIN_VALUE / 10) {
+                return 0; 
             }
-            
+
             result *= 10;
-            result += digitValue;
+            result += (x % 10) * sign;
             x /= 10;
         }
-        
+
         return result;
     }
 }
