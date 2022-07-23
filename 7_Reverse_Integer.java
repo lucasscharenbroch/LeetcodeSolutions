bc5f314eda87cc3e866b1b5f78a4f4d37dc88ab4
Lucas Scharenbroch <79030344+lucasscharenbroch@users.noreply.github.com>
2022-07-22 19:10:32 -0500

    Create 7_Reverse_Integer.java

---
+++ 7_Reverse_Integer.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int reverse(int x) {
+        int result = 0;    
+        int sign = (x > 0) ? 1 : -1;
+        
+        if(sign == -1) {
+            x = -x;
+        } 
+        
+        while(x != 0) {
+            if(result > Integer.MAX_VALUE / 10 || result < Integer.MIN_VALUE / 10) {
+                return 0; 
+            }
+            
+            result *= 10;
+            result += (x % 10) * sign;
+            x /= 10;
+        }
+        
+        return result;
+    }
+}
