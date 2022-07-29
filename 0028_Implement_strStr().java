59c14e49ac29b6a3fe668710fbf8023319ac2edf
Lucas Scharenbroch <79030344+lucasscharenbroch@users.noreply.github.com>
2022-07-28 19:04:03 -0500

    Create 0028_Implement_strStr().java

---
+++ 0028_Implement_strStr().java
@@ -0,0 +1,16 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for(int i = 0; i <= haystack.length() - needle.length(); i++) {
+            int j; // pointer to compare needle and haystack
+            for(j = 0; j < needle.length(); j++) {
+                if(needle.charAt(j) != haystack.charAt(i + j)) {
+                    break;
+                }
+            }    
+            
+            if(j == needle.length()) return i;
+        }
+        
+        return -1;
+    }
+}
