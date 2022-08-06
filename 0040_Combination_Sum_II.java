6b388dcbb5bd9a053b6820525b3284606d559367
Lucas Scharenbroch <79030344+lucasscharenbroch@users.noreply.github.com>
2022-08-06 18:26:02 -0500

    Update 0040_Combination_Sum_II.java
    cleaned up solution

--- 0040_Combination_Sum_II.java
+++ 0040_Combination_Sum_II.java
@@ -1,25 +1,26 @@
 class Solution {
-    private void findCombinations(int[] candidates, int start, int target, 
-                                  List<List<Integer>> solutions, List<Integer> combo) {
-        if(target == 0) { // found a solution
+    private ArrayList<List<Integer>> solutions = new ArrayList<List<Integer>>();
+    
+    private void findCombinations(int[] candidates, int target, int start, ArrayList<Integer> combo) {
+        if(target == 0) {
             solutions.add(new ArrayList<Integer>(combo));
         } else if(target > 0) {
-            // each iteration represents a 0-use, each recursion represents a 1-use
-            for(int i = start; i < candidates.length; i++) {
-                // do not recurse with duplicates
-                if(i != start && candidates[i] == candidates[i - 1]) continue;
-                combo.add(candidates[i]);
-                findCombinations(candidates, i + 1, target - candidates[i], solutions, combo);
-                combo.remove(combo.size() - 1);
-            }    
+            if(start == candidates.length) return;
+            int nextNonDuplicate;
+            // advance nextNonDuplicate
+            for(nextNonDuplicate = start + 1; nextNonDuplicate < candidates.length && 
+                                              candidates[start] == candidates[nextNonDuplicate];
+                                              nextNonDuplicate++);
+            findCombinations(candidates, target, nextNonDuplicate, combo); // 0
+            combo.add(candidates[start]);
+            findCombinations(candidates, target - candidates[start], start + 1, combo); // 1+
+            combo.remove(combo.size() - 1);
         }
     }
-
+    
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates, 0, candidates.length); // for duplicate-checking
-        
-        List<List<Integer>> solutions = new ArrayList<List<Integer>>();    
-        findCombinations(candidates, 0, target, solutions, new ArrayList<Integer>()); 
+        Arrays.sort(candidates, 0, candidates.length); // sort so duplicates can be handled
+        findCombinations(candidates, target, 0, new ArrayList<Integer>());
         return solutions;
     }
 }
