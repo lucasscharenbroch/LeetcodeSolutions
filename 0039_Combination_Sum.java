83956af9313ec58eeabf34149113ddedf527a16a
Lucas Scharenbroch <79030344+lucasscharenbroch@users.noreply.github.com>
2022-08-06 18:11:20 -0500

    Update 0039_Combination_Sum.java
    cleaned up solution

--- 0039_Combination_Sum.java
+++ 0039_Combination_Sum.java
@@ -1,45 +1,18 @@
 class Solution {
-    // globals
-    private int sum = 0;
-    private ArrayList<Integer> combination = new ArrayList<Integer>();
     private ArrayList<List<Integer>> solutions = new ArrayList<List<Integer>>();
-    
-    // adds all possible combinations of candidates (excluding indices left of start) 
-    // that add up to target - sum
-    private void findCombinations(int[] candidates, int start, int target) {
-        if(start == candidates.length) return;
-        int c = candidates[start];
-        int n = 0; // the number of times candidates[start] is used
-        do {
-            if(sum == target) {
-                solutions.add((List<Integer>) combination.clone());
-                break;
-            } else if(sum < target) {
-                findCombinations(candidates, start + 1, target);
-            }
-            combination.add(c);
-            sum += c;
-            n++;
-        } while(sum <= target);
-        
-        // remove footprint on sum and combiantion
-        sum -= n * c;
-        while(n-- > 0) {
-            combination.remove(combination.size() - 1);    
+    private void findCombinations(int[] candidates, int target, int start, ArrayList<Integer> combo) {
+        if(target == 0) { // combo is a correct solution
+            solutions.add(new ArrayList<Integer>(combo));
+        } else if(target > 0) {
+            if(start == candidates.length) return;
+            findCombinations(candidates, target, start + 1, combo); // 0
+            combo.add(candidates[start]);
+            findCombinations(candidates, target - candidates[start], start, combo); // >= 1
+            combo.remove(combo.size() - 1);
         }
     }
-    
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        // find the combinations starting with each candidate
-        for(int i = 0; i < candidates.length; i++) {
-            int c = candidates[i];
-            combination.add(c);
-            sum += c;
-            findCombinations(candidates, i, target);
-            sum -= c;
-            combination.remove(combination.size() - 1);
-        }
-        
+        findCombinations(candidates, target, 0, new ArrayList<Integer>());
         return solutions;
     }
 }
