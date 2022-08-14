abacdc4692903151cfa43b2d33c581cd775532a8
Lucas Scharenbroch <79030344+lucasscharenbroch@users.noreply.github.com>
2022-08-14 18:10:00 -0500

    Update 0060_Permutation_Sequence.java
    cleaned up solution & added comments

--- 0060_Permutation_Sequence.java
+++ 0060_Permutation_Sequence.java
@@ -1,34 +1,36 @@
 class Solution {
-    private int calculateFactorial(int n) {
-        if(n <= 1) return 1;    
-        else return n * calculateFactorial(n - 1);
+    // calculates n!
+    private int factorial(int n) {
+        if(n == 1) return 1;
+        else return n * factorial(n - 1);
     }
+    
     
     LinkedList<Integer> permutation = new LinkedList<>();
     
-    private void findRest(int n, int k) {
-        int factorial = calculateFactorial(n - 1);
-        if(k == 1) {
-            for(int i = 1; i <= n; i++) permutation.add(i); // add 1...n to permutation
-        } else if(k == factorial * n) {
-            for(int i = n; i >= 1; i--) permutation.add(i); // add n...1 to permutation
+    // sets permutation to the kth permutation of 1...n, where (k = 0) => (1...n) 
+    private void permuteRest(int n, int k) {
+        if(k == 0) {
+            for(int i = 0; i < n; i++) permutation.add(i + 1);
         } else {
-            findRest(n - 1, (k % factorial == 0 ? factorial : k % factorial)); // calculate the rest of the permation
-            int firstElement = ((k - 1) / factorial) + 1;
-            permutation.add(0, firstElement);
-            // scale the rest of the permutation up
-            for(int i = 1; i < permutation.size(); i++) {
-                // increment all elements >= permutation[0]
+            int lastFactorial = factorial(n - 1);
+            permuteRest(n - 1, k % lastFactorial);
+            int firstElement = (k / lastFactorial) + 1;
+            
+            // increment every element that is >= firstElement
+            for(int i = 0; i < permutation.size(); i++) {
                 if(permutation.get(i) >= firstElement) permutation.set(i, permutation.get(i) + 1);
             }
+            
+            permutation.add(0, firstElement); // insert firstElement at beginning of permutation
         }
     }
     
     public String getPermutation(int n, int k) {
-        findRest(n, k);
-        String result = "";
+        permuteRest(n, k - 1); // k = 1 -> k = 0
         
-        // convert permutation to a string
+        // convert permutation to string
+        String result = new String();
         for(int i = 0; i < permutation.size(); i++) result += permutation.get(i).toString();
         
         return result;
