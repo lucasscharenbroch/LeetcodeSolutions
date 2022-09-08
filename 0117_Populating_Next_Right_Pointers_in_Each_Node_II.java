c49cc5edc968ecbcb39fe14266913365856ed82e
Lucas Scharenbroch <79030344+lucasscharenbroch@users.noreply.github.com>
2022-09-07 19:53:45 -0500

    Create 0117_Populating_Next_Right_Pointers_in_Each_Node_II.java

---
+++ 0117_Populating_Next_Right_Pointers_in_Each_Node_II.java
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) return null; // empty tree edge-case
+        ArrayList<Node> bfs = new ArrayList<>();
+        
+        bfs.add(root);
+        bfs.add(null);  // null signifies a new level
+        
+        // do a bfs, setting the next pointers to the next element in the bfs arrayList.
+        for(int i = 0; i < bfs.size() - 1; i++) {
+            if(bfs.get(i) == null) {
+                bfs.add(null);
+                continue;
+            }
+            
+            bfs.get(i).next = bfs.get(i + 1);
+            
+            if(bfs.get(i).left != null) bfs.add(bfs.get(i).left);
+            if(bfs.get(i).right != null) bfs.add(bfs.get(i).right);
+        }
+        
+        return root;
+    }
+}
