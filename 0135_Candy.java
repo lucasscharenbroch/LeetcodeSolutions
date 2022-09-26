5d92849ad7bfbc7d3d30db5103b550999161c62b
Lucas Scharenbroch <79030344+lucasscharenbroch@users.noreply.github.com>
2022-09-26 16:44:40 -0500

    Update 0135_Candy.java
    corrected solution

--- 0135_Candy.java
+++ 0135_Candy.java
@@ -10,15 +10,12 @@ class Solution {
             int leftRating = (i == 0) ? Integer.MAX_VALUE : ratings[i - 1];
             int rightRating = (i == ratings.length - 1) ? Integer.MAX_VALUE : ratings[i + 1];
             
-            System.out.printf("i: %d , ir: %d, lr %d rr %d\n", i, ratings[i], leftRating, rightRating);
-
-            
             if(ratings[i] <= leftRating && ratings[i] <= rightRating) { // MINIMUM
                 // no extra candy
             } else if(ratings[i] > leftRating && ratings[i] <= rightRating) { // UPWARD SLOPING
                 total += extraCandy[i] = extraCandy[i - 1] + 1;
             } else if(ratings[i] > leftRating && ratings[i] > rightRating) { // MAXIMUM
-                stack.add(new int[] {i, ratings[i - 1]}); // add max(r[i-1], r[i+1])
+                stack.add(new int[] {i, extraCandy[i - 1] + 1}); // add max(r[i-1] + 1, r[i+1] + 1)
             } else { // downward sloping
                 stack.add(new int[] {i, 0});
             }
@@ -26,14 +23,9 @@ class Solution {
         
         while(!stack.isEmpty()) {
             int[] current = stack.remove(stack.size() - 1);
-            System.out.println("current = " + current[0] + " " + current[1]);
             int numCandy = Math.max(current[1], extraCandy[current[0] + 1] + 1);
             total += numCandy;
             extraCandy[current[0]] = numCandy;
-        }
-        
-        for(int i = 0; i < ratings.length; i++) {
-            System.out.println(i + ": " + extraCandy[i]);
         }
         
         return total;
