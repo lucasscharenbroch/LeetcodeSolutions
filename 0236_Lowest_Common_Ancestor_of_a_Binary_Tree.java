7e057a0c39ae4e9122ba052a681b2082b86de376
Lucas Scharenbroch <79030344+lucasscharenbroch@users.noreply.github.com>
2022-10-19 15:16:20 -0500

    Create 0236_Lowest_Common_Ancestor_of_a_Binary_Tree.java

---
+++ 0236_Lowest_Common_Ancestor_of_a_Binary_Tree.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    // this function returns one of: {null, p, q, lca}
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        if(left != null && right != null) return root; // found lca
+        return (left != null) ? left : right; // return the found node, if any
+    }
+}
