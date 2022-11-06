077d573c2695ccc6d97fb1917594719d87a2fd62
Lucas Scharenbroch <79030344+lucasscharenbroch@users.noreply.github.com>
2022-11-06 09:02:36 -0600

    Update 0239_Sliding_Window_Maximum.java

--- 0239_Sliding_Window_Maximum.java
+++ 0239_Sliding_Window_Maximum.java
@@ -18,3 +18,33 @@ class Solution {
         return result;
     }
 }
+
+// Dequeue, O(n)
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        // q is a dequeue that contains a decreasing subsequence of the current window
+        // therefore, at any time, the maximum is the first element of q
+        LinkedList<Integer> q = new LinkedList<Integer>();
+        
+        // fill q with initial window
+        for(int i = 0; i < k; i++) {
+            while(q.peekLast() != null && q.peekLast() < nums[i]) q.removeLast();
+            q.addLast(nums[i]);
+        }
+            
+        int left = 0, right = k;
+        int[] result = new int[nums.length - k + 1];
+        for(int i = 0; right <= nums.length; i++) {
+            result[i] = q.getFirst();
+            
+            if(nums[left] == q.getFirst()) q.removeFirst(); // remove left (if necessary)
+            if(right == nums.length) break;
+            while(q.peekLast() != null && q.peekLast() < nums[right]) q.removeLast(); // add right
+            q.addLast(nums[right]);
+            
+            left++; right++;
+        }
+        
+        return result;
+    }
+}
