4818f296205ca24c87a0b9ca0d29cfac276c17e5
Lucas Scharenbroch <79030344+lucasscharenbroch@users.noreply.github.com>
2022-11-19 21:09:23 -0600

    Create 1528_Shuffle_String.java

---
+++ 1528_Shuffle_String.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] result = new char[s.length()];
+        
+        for(int i = 0; i < indices.length; i++) {
+            result[indices[i]] = s.charAt(i);
+        }
+        
+        return String.valueOf(result);
+    }
+}
