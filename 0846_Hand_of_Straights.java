842c06202409f2ee8f36597bc81f443a0379e882
Lucas Scharenbroch <79030344+lucasscharenbroch@users.noreply.github.com>
2022-11-25 09:01:59 -0600

    Update 0846_Hand_of_Straights.java

--- 0846_Hand_of_Straights.java
+++ 0846_Hand_of_Straights.java
@@ -14,7 +14,7 @@ class Solution {
             int numStraights = cardCounts.get(card); // num straights that must be formed with card as first card
             if(numStraights != 0) {
                 for(int i = 0; i < groupSize; i++) {
-                    if(cardCounts.computeIfAbsent(card + i, (k) -> 0) < numStraights) return false;
+                    if(cardCounts.getOrDefault(card + i, 0) < numStraights) return false;
                     cardCounts.put(card + i, cardCounts.get(card + i) - numStraights);
                 }
             }
