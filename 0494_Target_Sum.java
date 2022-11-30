723622bf117b8a9f5635342b30b459d26970314e
Lucas Scharenbroch <79030344+lucasscharenbroch@users.noreply.github.com>
2022-11-30 12:28:36 -0600

    Update 0494_Target_Sum.java

--- 0494_Target_Sum.java
+++ 0494_Target_Sum.java
@@ -11,3 +11,29 @@ class Solution {
                findTargetSumWays(nums, start + 1, target + nums[start]);
     }
 }
+
+// DP, O(40,001 * n) = O(n)
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        final int MAX_ELEMENT = 1000;
+        final int MAX_NUMS_LENGTH = nums.length;
+        final int MAX_SUM = MAX_ELEMENT * MAX_NUMS_LENGTH;
+        
+        int[][] dp = new int[nums.length + 1][MAX_SUM * 2 + 1]; // dp[i][j] stores the number of ways
+                                                                // to sum to j using nums[0..i]
+        // dp is shifted down MAX_SUM (dp[0] ~> -20,000, dp[MAX_SUM] ~> 0, dp[MAX_SUM * 2 + 1] ~> 20,000)
+        
+        dp[0][MAX_SUM] = 1; // one way to sum to 0.
+        
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 0; j < MAX_SUM * 2 + 1; j++) {
+                if(dp[i][j] != 0) {
+                    dp[i + 1][j + nums[i]] += dp[i][j]; // +
+                    dp[i + 1][j - nums[i]] += dp[i][j]; // -
+                }
+            }
+        }
+        
+        return dp[nums.length][target + MAX_SUM];
+    }
+}
