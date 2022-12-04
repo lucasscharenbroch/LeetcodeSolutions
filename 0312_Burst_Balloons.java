7cade8940999515971f3bad74af3505acb079483
Lucas Scharenbroch <79030344+lucasscharenbroch@users.noreply.github.com>
2022-12-04 15:45:06 -0600

    Update 0312_Burst_Balloons.java

--- 0312_Burst_Balloons.java
+++ 0312_Burst_Balloons.java
@@ -1,4 +1,4 @@
-// Top-Down Memoization, O(n^3)
+// Top-down Memoization, O(n^3)
 class Solution {
     public int maxCoins(int[] nums) {
         int[][] memo = new int[nums.length][nums.length];
@@ -27,5 +27,46 @@ class Solution {
             
         memo[start][end] = score;
         return score;
+    }
+}
+
+// Bottom-up DP, O(n^3)
+class Solution {
+    public int maxCoins(int[] nums) {
+        int[][] dp = new int[nums.length][nums.length]; // dp[i][j] = max score on range [i, j]- assume
+                                                        // that this range is the first popped.
+        
+        // if we find the best score for all ranges of length n, 
+        // we can use that to find all ranges of (n + 1)
+        
+        // get ranges of length 1 (score for popping any given balloon first)
+        for(int i = 0; i < nums.length; i++) {
+            dp[i][i] = ((i == 0) ? 1 : nums[i - 1]) *              // balloon (i - 1) or 1
+                       nums[i] *                                   // balloon i
+                       ((i == nums.length - 1) ? 1 : nums[i + 1]); // ballon to (i + 1) or 1
+        }
+        
+        for(int n = 2; n <= nums.length; n++) {
+            for(int i = 0; i + n <= nums.length; i++) {
+                // find max score of the window [i, i + n - 1]
+                int score = 0;
+                int s = i, e = i + n - 1; // start and end
+                
+                int leftOuter = (s == 0) ? 1 : nums[s - 1]; // nums[s - 1] or 1
+                int rightOuter = (e == nums.length - 1) ? 1 : nums[e + 1]; // nums[e + 1] or 1
+                
+                for(int k = s; k <= e; k++) { // consider every possible last balloon to pop in the range
+                    int scoreWithKAsFirst = nums[k] * leftOuter * rightOuter;
+                    if(k == s) scoreWithKAsFirst += dp[s + 1][e];
+                    else if(k == e) scoreWithKAsFirst += dp[s][e - 1];
+                    else scoreWithKAsFirst += dp[s][k - 1] + dp[k + 1][e];
+                    score = Math.max(score, scoreWithKAsFirst);
+                }
+                    
+                dp[s][e] = score;
+            }
+        }
+        
+        return dp[0][nums.length - 1];
     }
 }
