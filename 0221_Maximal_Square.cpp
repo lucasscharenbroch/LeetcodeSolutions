e20110c3d2c11f1324da0909bf17d0fe13475be0
Lucas Scharenbroch <79030344+lucasscharenbroch@users.noreply.github.com>
2023-01-23 11:01:34 -0600

    Update 0221_Maximal_Square.cpp

--- 0221_Maximal_Square.cpp
+++ 0221_Maximal_Square.cpp
@@ -36,3 +36,37 @@ public:
         return l * l;
     }
 };
+
+// DP (O(n * m))
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        // three 2d DP arrays:
+        int dpSquare[300][300] = {0};   // largest square whose bot-right corner = (i, j)
+        int dpVertLine[300][300] = {0}; // longest vertical line whose bot point is (i, j)
+        int dpHorzLine[300][300] = {0}; // longest horizontal line whose bot point is (i, j)
+        
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        int largestSquareSide = 0;
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(i == 0) dpVertLine[i][j] = matrix[i][j] == '1';
+                else dpVertLine[i][j] = matrix[i][j] == '0' ? 0 : 1 + dpVertLine[i - 1][j];
+                
+                if(j == 0) dpHorzLine[i][j] = matrix[i][j] == '1';
+                else dpHorzLine[i][j] = matrix[i][j] == '0' ? 0 : 1 + dpHorzLine[i][j - 1];
+                
+                if(i == 0 || j == 0) dpSquare[i][j] = matrix[i][j] == '1';
+                else if(matrix[i][j] == '0') dpSquare[i][j] = 0;
+                else dpSquare[i][j] = 1 + min(dpSquare[i - 1][j - 1], min(dpVertLine[i - 1][j],
+                                                                      dpHorzLine[i][j - 1]));
+                largestSquareSide = max(largestSquareSide, dpSquare[i][j]);
+            }
+        }
+        
+        return largestSquareSide * largestSquareSide;
+    }
+};
