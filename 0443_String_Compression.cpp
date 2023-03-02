6cc87fcf7e9b1fa7878ea94c8aa96c228d5010f2
Lucas Scharenbroch <79030344+lucasscharenbroch@users.noreply.github.com>
2023-03-02 12:22:20 -0600

    Update 0443_String_Compression.cpp

--- 0443_String_Compression.cpp
+++ 0443_String_Compression.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int count = 1;
         int pos = 0; // writing position in chars
         int i = 0; // reading position in chars
         
         while(i < chars.size()) {
             chars[pos++] = chars[i++]; // write first unique char
-            count = 1;
+            int count = 1;
             while(i != chars.size() && chars[i] == chars[i - 1]) i++, count++; // count duplicates
             if(count != 1) for(char j : to_string(count)) chars[pos++] = j; // write count (if count > 1)
         }
