614789204df504f388761f998e87aa7c2c09e591
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2024-04-22 15:06:17 -0500

    Create 0042_Trapping_Rain_Water.py

---
+++ 0042_Trapping_Rain_Water.py
@@ -0,0 +1,12 @@
+from itertools import accumulate
+from operator import sub
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left = accumulate(height, max)
+        right = reversed(list(accumulate(reversed(height), max)))
+        boundaries = map(min, zip(left, right))
+        water = map(lambda x: x[0] - x[1], zip(boundaries, height))
+        return sum(water)
+        
+        
